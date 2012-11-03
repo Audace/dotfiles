5263619f5ab97c89f6cecf1018c8d4027c061439
Ben Standefer <benstandefer@gmail.com>
2012-11-02 23:56:05 -0700

    Set font in emacs.

--- .emacs.d/ben.el
+++ .emacs.d/ben.el
@@ -1,6 +1,11 @@
-(setq mac-command-modifier 'meta) ; sets the Command key as Meta
+;; sets the Command key as Meta
+(setq mac-command-modifier 'meta)
 
+;; Make Cmd-V still paste.
 ;; http://hints.macworld.com/article.php?story=20091013214429696
 (setq ns-alternate-modifier 'alt)
 (define-key global-map [(alt ?v)] 'scroll-down)
 (define-key global-map [(meta ?v)] 'yank)
+
+;; Set font to Inconsolata-14
+(set-default-font "Inconsolata-14")
