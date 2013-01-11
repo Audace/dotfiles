67ee00786cb048b7a42c8f340786413017a213bd
Ben Standefer <benstandefer@gmail.com>
2013-01-11 14:11:39 -0800

    SSH in Emacs was not working due to new ZSH prompt, go back to default ZSH prompt for now.

--- .emacs.d/ben.el
+++ .emacs.d/ben.el
@@ -1,104 +1,9 @@
-;; [4:28] Ian Eure: You will need these things: http://gist.github.com/457925
-;; [4:28] Ian Eure: And http://gist.github.com/457933
-;; [4:28] Ian Eure: Then M-x ssh RET hostname RET
-;; [4:28] Ian Eure: Instead of M-x shell RET ssh hostname RET
-;; [4:37] Benjamin Standefer: is the second gist also in ~/.emacs.d/ssh.el/
-;; [4:37] Benjamin Standefer: err, ~/.emacs.d/ssh.el
-;; [4:38] Ian Eure: No, you want it in ~/.emacs.d/init.el
-;; [4:38] Ian Eure: The first form won't do anything. You want to M-x customize-variable that.
-;; [4:38] Ian Eure: Also, I can't stop listening to http://www.youtube.com/watch?v=bnCHCcveteA
-;; [4:39] Benjamin Standefer: "first form" being lines 4-14 of the 2nd gist?
-;; [4:39] Ian Eure: ssh-directory-tracking-mode
-;; [4:39] Ian Eure: Is what you want to customize. Then remove that and the comment above.
-;; [4:40] Benjamin Standefer: so that sets it up to be t?
-;; [4:40] Ian Eure: When you customize it, you want to set it to t.
-;; [4:40] Ian Eure: M-x customize-variable RET ssh-directory-tracking-mode RET
-;; [4:41] Ian Eure: And for some reason… Uh.
-;; [4:41] Ian Eure: You want to choose "ftp" from that list.
-;; [4:41] Ian Eure: Don't ask why, it makes no sense.
-;; [4:44] Benjamin Standefer: so the first 2 lines of the 2nd gist I do not paste into init.el
-;; [4:44] Ian Eure: Correct.
-;; [4:44] Benjamin Standefer: just the hooks
-;; [4:44] Benjamin Standefer: ok
-;; [4:44] Ian Eure: Yes.
-
-(require 'ssh)
-
-(eval-after-load 'ssh
-  '(progn
-     (add-hook 'ssh-mode-hook
-               (lambda ()
-                 (shell-dirtrack-mode t)
-                 (setq dirtrackp nil)
-                 (setq show-trailing-whitespace nil)))
-
-     (defadvice ssh (around ssh-always-local first activate)
-       (let ((default-directory "~/"))
-         ad-do-it))))
-
-(eval-after-load 'shell
-    '(progn
-       (defadvice comint-send-input (before expand-input activate)
-         "Expand input before sending"
-         (expand-abbrev))
-       (add-hook 'shell-mode-hook
-                 (lambda ()
-                   (ansi-color-for-comint-mode-on)
-                   (setq shell-dirtrackp nil)
-                   (dirtrack-mode t)
-                   (setq show-trailing-whitespace nil)))))
+;; Sets the Command key as Meta
+(setq mac-command-modifier 'meta)
 
 
-;; Stuff to pimp ibuffer from Paul Lathrop
-;;; ibuffer
-(require 'ibuffer)
-(setq ibuffer-expert t
-      ibuffer-show-empty-filter-groups nil)
-(setq ibuffer-saved-filter-groups
-      '(("default"
-         ("Dired" (mode . dired-mode))
-         ("Shell Script" (mode . shell-script-mode))
-         ("SSH" (mode . ssh-mode))
-         ("Ruby" (mode . ruby-mode))
-         ("Python" (mode . python-mode))
-         ("Puppet" (mode . puppet-mode))
-         ("Magit" (name . "magit:"))
-         ("Tramp" (name . "*tramp"))
-         ("ERC" (mode . erc-mode))
-         ("Emacs" (name . "\\*.*\\*")))))
-(add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (ibuffer-auto-mode 1)
-            (ibuffer-switch-to-saved-filter-groups "default")))
-
-
-;; Enable winner-mode to switch between window configurations using
-;; C-c + arrow
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
-
-
-;; Custom variables
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-
-  ;; For this to work, your prompt needs to be set to export PS1='\h!\u:\w\$ '
- '(dirtrack-list (quote ("\\((.+)\\)?[a-z0-9]+.[a-z0-9]+:\\(.*\\)[$#] $" 2)))
-
- '(explicit-bash-args (quote ("--noediting" "-i" "-l")))
- '(ido-mode nil nil (ido))
- '(iswitchb-mode t)
- '(ns-command-modifier (quote meta))
- '(ssh-directory-tracking-mode t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+;; Set font to Inconsolata-14
+(set-default-font "Inconsolata-14")
 
 
 ;; Make connecting to servers easier
@@ -131,17 +36,3 @@
           ssh-connection-alist))
 
 (ssh-convenience)
-
-
-;; Make TextExpander work with Emacs on OS X
-;; http://hints.macworld.com/article.php?story=20091013214429696
-(setq ns-alternate-modifier 'alt)
-(define-key global-map [(alt ?v)] 'scroll-down)
-(define-key global-map [(meta ?v)] 'yank)
-
-;; sets the Command key as Meta
-;; not sure if this is required
-;; (setq mac-command-modifier 'meta)
-
-;; Set font to Inconsolata-14
-(set-default-font "Inconsolata-14")
